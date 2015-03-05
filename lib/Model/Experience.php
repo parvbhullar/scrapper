d1cbb57e55591d187a0b09d551e7b19090cd4d68
lakhvinder singh <ldandiwal@gmail.com>
2015-03-05 15:20:59 +0530

    [Model] new added

--- lib/Model/Experience.php
+++ lib/Model/Experience.php
@@ -8,7 +8,7 @@ use Purekid\Mongodm\Model;
  *
  * @final
  */
-final class Education extends Model
+final class Experience extends Model
 {
     public $excluded = array('_id', 'created_by', 'created_at');
     public function Initialize($user = "Unknown"){
@@ -16,20 +16,23 @@ final class Education extends Model
         $this->created_at = time();
         $this->created_by = $user;
     }
-    static $collection = "Education";
+    static $collection = "Experience";
 
     /** specific definition for attributes, not necessary! **/
     protected static $attrs = array(
-
         'profile' => array('model'=> 'Model\\Profiles', 'type'=>'reference'), //If single reference den use embed
-        'school' => array('default'=>'','type'=>'string'),
-        'degree' => array('default'=>'','type'=>'string'),
-        'program' => array('default'=>'','type'=>'string'),
+
+        'role' => array('default'=>'','type'=>'string'),
+        'companyName' => array('default'=>'','type'=>'string'),
+        '$industry' => array('default'=>'','type'=>'string'),
         'fromdate' => array('type'=>'timestamp'),
         'todate' => array('type'=>'timestamp'),
-        'year' => array('default'=>'','type'=>'string'),
-        'gpa' => array('default'=>'','type'=>'string'),
-        'seq' => array('default'=>'','type'=>'integer'),
+        'duration' => array('default'=>'','type'=>'string'),
+        'current' => array('type'=>'boolean'),
+        'location' => array('default'=>'','type'=>'string'),
+        'description' => array('default'=>'','type'=>'string'),
+        '$seq' => array('default'=>0,'type'=>'integer'),
+
         'status' => array('default'=>1, 'type'=>'integer'), #0 deactive, 1 active, 2 deleted
         'created_by' => array('default'=>'Unknown','type'=>'string'),
         'created_at' => array('type'=>'timestamp')
--- lib/Model/OutBoundProfileLinks.php
+++ lib/Model/OutBoundProfileLinks.php
@@ -8,7 +8,7 @@ use Purekid\Mongodm\Model;
  *
  * @final
  */
-final class Education extends Model
+final class OutBoundProfileLinks extends Model
 {
     public $excluded = array('_id', 'created_by', 'created_at');
     public function Initialize($user = "Unknown"){
@@ -16,20 +16,15 @@ final class Education extends Model
         $this->created_at = time();
         $this->created_by = $user;
     }
-    static $collection = "Education";
+    static $collection = "OutBoundProfileLinks";
 
     /** specific definition for attributes, not necessary! **/
     protected static $attrs = array(
-
         'profile' => array('model'=> 'Model\\Profiles', 'type'=>'reference'), //If single reference den use embed
-        'school' => array('default'=>'','type'=>'string'),
-        'degree' => array('default'=>'','type'=>'string'),
-        'program' => array('default'=>'','type'=>'string'),
-        'fromdate' => array('type'=>'timestamp'),
-        'todate' => array('type'=>'timestamp'),
-        'year' => array('default'=>'','type'=>'string'),
-        'gpa' => array('default'=>'','type'=>'string'),
-        'seq' => array('default'=>'','type'=>'integer'),
+        'name' => array('default'=>'','type'=>'string'),
+        'sourse' => array('default'=>'','type'=>'string'),
+        '$summary' => array('default'=>'','type'=>'string'),
+
         'status' => array('default'=>1, 'type'=>'integer'), #0 deactive, 1 active, 2 deleted
         'created_by' => array('default'=>'Unknown','type'=>'string'),
         'created_at' => array('type'=>'timestamp')
