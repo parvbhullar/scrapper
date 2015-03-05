6bced6f3c6ff33a544f908c16e93573c7c92c487
lakhvinder singh <ldandiwal@gmail.com>
2015-03-05 14:03:56 +0530

    profiles modify

---
+++ lib/Model/Education.php
@@ -0,0 +1,35 @@
+<?php
+namespace Model;
+use Purekid\Mongodm\Model;
+
+/**
+ *
+ * @package
+ *
+ * @final
+ */
+final class Education extends Model
+{
+    public $excluded = array('_id', 'created_by', 'created_at');
+    public function Initialize($user = "Unknown"){
+        $this->status = 1; // 0 Inactive, 1 Active, 2 Deleted
+        $this->created_at = time();
+        $this->created_by = $user;
+    }
+    static $collection = "Education";
+
+    /** specific definition for attributes, not necessary! **/
+    protected static $attrs = array(
+        'user_id' => array('default'=>'','type'=>'mixed'),
+        'topic_id' => array('default'=>'','type'=>'string'),
+        'url' => array('default'=>'','type'=>'string'),
+        'search_source' => array('default'=>'','type'=>'integer'),
+        'time' => array('type'=>'timestamp'),
+        'status' => array('default'=>1, 'type'=>'integer'), #0 deactive, 1 active, 2 deleted
+        'created_by' => array('default'=>'Unknown','type'=>'string'),
+        'created_at' => array('type'=>'timestamp')
+    );
+
+}
+
+?>
--- lib/Model/Profiles.php
+++ lib/Model/Profiles.php
@@ -20,11 +20,23 @@ final class Profiles extends Model
 
     /** specific definition for attributes, not necessary! **/
     protected static $attrs = array(
-        'user_id' => array('default'=>'','type'=>'mixed'),
-        'topic_id' => array('default'=>'','type'=>'string'),
-        'url' => array('default'=>'','type'=>'string'),
-        'search_source' => array('default'=>'','type'=>'integer'),
-        'time' => array('type'=>'timestamp'),
+        'name' => array('default'=>'','type'=>'string'),
+        'title' => array('default'=>'','type'=>'string'),
+        'gender' => array('default'=>'','type'=>'string'),
+        'sourse' => array('default'=>'','type'=>'string'),
+        'profileStore' => array('default'=>1,'type'=>'integer'), # 1 targetDocument="ProfileStore", 2 cascade="persist"
+        'industry' => array('default'=>'','type'=>'string'),
+        'locality' => array('default'=>'','type'=>'string'),
+        //** array field */
+        'outBoundProfilesLinks' => array('default'=>'','type'=>'string'), #targetDocument="OutBoundProfileLinks", cascade="persist"
+        'experience' => array('default'=>'','type'=>'string'), #targetDocument="Experience", cascade="persist"
+        'education' => array('default'=>'','type'=>'string'), #targetDocument="Education", cascade="persist"
+
+        'sourceService' => array('default'=>'','type'=>'string'),
+        'resumeLastUpdated' => array('type'=>'timestamp'),
+        'updatedInES' => array('default'=>'','type'=>'string'),
+        'shMetadata' => array('default'=>1,'type'=>'integer'), # 1 targetDocument="SHMetadata", 2 cascade="persist"
+
         'status' => array('default'=>1, 'type'=>'integer'), #0 deactive, 1 active, 2 deleted
         'created_by' => array('default'=>'Unknown','type'=>'string'),
         'created_at' => array('type'=>'timestamp')
